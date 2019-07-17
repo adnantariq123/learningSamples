da32d282a664358c67d00580c401187dc55e3da2
Adnan <adnan.tariq1980@gmail.com>
2019-07-17 11:09:38 -0400

    updated the display with original PLUS  added code for different samples of methods to run on array

--- _js/ArrayMethods.js
+++ _js/ArrayMethods.js
@@ -1,42 +1,45 @@
 const items = [
-  { name: "Bike", price: 100 },
-  { name: "TV", price: 700 },
-  { name: "Album", price: 10 },
-  { name: "Book", price: 5 },
-  { name: "Phone", price: 500 },
-  { name: "Computer", price: 1000 },
-  { name: "Keyboard", price: 25 }
+  { namezz: "Bike", price: 100 },
+  { namezz: "TV", price: 700 },
+  { namezz: "Album", price: 10 },
+  { namezz: "Book", price: 5 },
+  { namezz: "Phone", price: 500 },
+  { namezz: "Computer", price: 1000 },
+  { namezz: "Keyboard", price: 25 }
 ];
 
 
 makeTableHTML = (myArray) => {
-    //let result = "<tbody>";
-    let result;
+    let result = "<tbody>";
+    //let result;
     for(var i=0; i<myArray.length; i++) {
-        console.log("result : "+result);
+        
         result += "<tr>";
 
-        /*
-        // THIS WORKS, But might not be the better appraoch 
-        // IMP look at what .entries for us Vs .keys... play with it ^^
-        loved = Object.entries(myArray[i]);
+        
+
+        // VERY IMPORTANT
+        // look at what .entries does Vs .keys Vs .values  ... play with it ^^
+        
+        loved = Object.values(myArray[i]);
         //console.log("loved onces :"+loved);
 
         for(var j=0; j<loved.length; j++){
             //console.log("did we come to myArray[i] :"+loved);
             result += "<td>"+loved[j]+"</td>";
-        }*/
+        }
         
 
-        result += Object.keys(myArray[i]).map(function(key) {
-        return ("<td>"+ myArray[i][key] + "</td>");
-        });
+        // THIS WORKS, But might not be the better appraoch 
+        // result += Object.keys(myArray[i]).map(function(key) {
+        // return ("<td>"+ myArray[i][key] + "</td>");
+        // });
 
         
         result += "</tr>";
     }
     result += "</tbody>";
-
+    //console.log("result : "+result);
     return result;
 }
 
@@ -46,6 +49,64 @@ window.addEventListener("load", () => {
 });
 
 
+
+
+
+// get all items in array less than or equal to $100 price
+const filteredItems =  items.filter((item) => {
+    return item.price <= 100
+})
+
+console.log(filteredItems);
+
+
+
+
+// map allows you to take an array and convert it into a new array that is slight different
+const itemNames =  items.map((item) => {
+    return item.namezz
+})
+
+console.log(itemNames);
+
+
+
+// self explanotory 
+const foundItem = items.find((item) => {
+    return item.namezz ==='Book';
+})
+
+console.log("found the item using the .find method "+ foundItem);
+
+
+
+
+
+items.forEach((item) => {
+    console.log(item.namezz)
+});
+
+
+// this goes to check if the array has ANY items less than 100, if yes then it returns true
+const inexpensiveItems = items.some((item) => {
+    return item.price <= 100;
+})
+
+console.log(inexpensiveItems);
+
+
+
+
+// So reduce method will take the array and bring it to a single value, with whatever maths you need it to do
+const total = items.reduce((currentTotal, item) => {
+    return item.price + currentTotal;
+}, 0) // 0 here was the starting value of currentTotal;
+
+console.log(total);
+
+
+
+const inclues
 
 
 
