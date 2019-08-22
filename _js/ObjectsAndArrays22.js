0837e26b874129453426bdd753ab965bb3e87d99
Adnan <adnan.tariq1980@gmail.com>
2019-08-22 10:47:42 -0400

    jquery is our lord and Saviour!

---
+++ _js/ObjectsAndArrays22.js
@@ -0,0 +1,33 @@
+
+
+$( document ).ready(function() {
+    console.log( "ready!" );
+
+var result1;
+var result2;
+$.when(
+    $.ajax({ // First Request
+        url: "/_js/someitems.json", 
+        method:"GET",
+        success: function(data){     
+                result1 = data;                  
+        }           
+    }),
+
+    $.ajax({ //Seconds Request
+        url: "/_js/someMoreItems.json", 
+        method:"GET",
+        success: function(data){                          
+            result2 = data;     
+        }           
+    })
+
+).then(function() {
+    var newObj = [...result1.items, ...result2.items];
+    // var newObj = {"items": []};
+    // newObj.items = [...result1, ...result2];
+    console.log(newObj);
+});
+
+
+});
