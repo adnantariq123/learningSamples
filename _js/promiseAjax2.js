992b94ad66f309666c927762c3a89acb7e92aebe
Adnan <adnan.tariq1980@gmail.com>
2019-08-28 10:15:45 -0400

    edits

--- _js/promiseAjax2.js
+++ _js/promiseAjax2.js
@@ -1,20 +1,31 @@
 
 
-function loadDoc(location) {
-    return new Promise (function ( resolve,reject) {
-      var xyz = new XMLHttpRequest();
-      xyz.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {
-          resolve(this.responseText);          
-        }
-      };
-      xyz.open("GET", location, true);
-      xyz.send();
-    });
-  }
-  
-  const urls = ["/_js/someitems.json", "/_js/someMoreItems.json"]
-  Promise.all(urls.map(url=>loadDoc(url))).then(responses =>
-    
-    console.log(responses)
-  )
+function myLoadDoc(location) {
+  return new Promise(function (resolve, reject) {
+
+    var xyz = new XMLHttpRequest();
+
+    xyz.onreadystatechange = function () {
+      if (this.readyState == 4 && this.status == 200) {
+        var wut = JSON.parse(this.responseText);
+        wut = wut.items;
+        resolve(wut);
+      }
+    };
+    xyz.open("GET", location, true);
+    xyz.send();
+  });
+}
+
+const urls = [
+  "/_js/someitems.json", 
+  "/_js/someMoreItems.json"
+]
+
+Promise.all(urls.map(
+  url => myLoadDoc(url))
+  ).then(responses =>
+
+  console.log(responses)
+  //console.log(typeof responses)
+)
