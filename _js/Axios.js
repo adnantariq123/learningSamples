c3d6a4f1e1ada1b6e14d54f4e023329caec66d93
adnantariq123 <ahmedadnan123@hotmail.com>
2024-10-17 11:01:52 -0400

    started with HTMLand5

--- _js/Axios.js
+++ _js/Axios.js
@@ -1,15 +1,15 @@
 
 const getBtn = document.getElementById("get-btn");
-const postBtn =  document.getElementById("post-btn");
+const postBtn = document.getElementById("post-btn");
 
 let x = document.getElementById("stuff");
 
 const getData = () => {
     axios.get('https://reqres.in/api/users')
-    .then(response=> {
-        console.log(response);
-        x.innerHTML = makeTableHTML(response.data.data);    
-    });
+        .then(response => {
+            console.log(response);
+            x.innerHTML = makeTableHTML(response.data.data);
+        });
 
 
 
@@ -18,7 +18,7 @@ const getData = () => {
 
 
 const sendData = () => {
-    
+
     axios.post('https://reqres.in/api/register', {
         // so even tho its an js object.. the post failed to send it across without the key-value-pais been in ""
         // in other words email and password HAD to be "email" "password"
@@ -28,7 +28,7 @@ const sendData = () => {
         // this was meant to happan as per the youtube https://www.youtube.com/watch?v=qM4G1Ai2ZpE
         "email": "eve.holt@reqres.in",
         "password": "pistol"
-    }).then(response=> {
+    }).then(response => {
         console.log(response);
 
         // fuck this part
@@ -58,30 +58,37 @@ postBtn.addEventListener('click', sendData);
 makeTableHTML = (myArray) => {
     let result = "<tbody>";
     //let result;
-    for(var i=0; i<myArray.length; i++) {
-        
+    for (var i = 0; i < myArray.length; i++) {
+
         result += "<tr>";
 
-        
+
 
         // VERY IMPORTANT
         // look at what .entries does Vs .keys Vs .values  ... play with it ^^
-        
+
         loved = Object.values(myArray[i]);
-        //console.log("loved onces :"+loved);
+        console.log("loved onces :" + loved);
 
-        for(var j=0; j<loved.length; j++){
-            //console.log("did we come to myArray[i] :"+loved);
-            result += "<td>"+loved[j]+"</td>";
+        for (var j = 0; j < loved.length; j++) {
+
+            //if (loved.at(-1).includes("https")) {
+            if (j === loved.length - 1) {
+                result += "<td> <img src=" + loved[j] + "></td>"
+
+            } else {
+                result += "<td>" + loved[j] + "</td>";
+            }
+
         }
-        
 
-        // THIS WORKS, But might not be the better appraoch 
+
+        //THIS WORKS, But might not be the better appraoch 
         // result += Object.keys(myArray[i]).map(function(key) {
         // return ("<td>"+ myArray[i][key] + "</td>");
         // });
 
-        
+
         result += "</tr>";
     }
     result += "</tbody>";
